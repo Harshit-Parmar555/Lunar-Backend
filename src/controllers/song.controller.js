a80dd32a366b48b4b2bc16e946edb955e67f7101
Harshit_Parmar <parmarharshit441@gmail.com>
2025-09-21 22:00:04 +0530

    setup songs route and controller

---
+++ src/controllers/song.controller.js
@@ -0,0 +1,111 @@
+import Song from "../models/song.model.js";
+
+export const getAllSongs = async (req, res) => {
+  try {
+    const songs = await Song.find().sort({ createdAt: -1 });
+    res.status(200).json({
+      success: true,
+      message: "Songs fetched successfully",
+      songs,
+    });
+  } catch (error) {
+    console.error("Error in fetching all songs:", error);
+    res.status(500).json({
+      success: false,
+      message: "An unexpected error occurred while fetching all songs.",
+    });
+  }
+};
+
+export const getFeaturedSongs = async (req, res) => {
+  try {
+    const songs = await Song.aggregate([
+      {
+        $sample: { size: 4 },
+      },
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          artist: 1,
+          coverImage: 1,
+          duration: 1,
+          url: 1,
+        },
+      },
+    ]);
+    res.status(200).json({
+      success: true,
+      message: "Featured songs fetched successfully",
+      songs,
+    });
+  } catch (error) {
+    console.error("Error in fetching features songs:", error);
+    res.status(500).json({
+      success: false,
+      message: "An unexpected error occurred while fetching featured songs.",
+    });
+  }
+};
+
+export const trendingSongs = async (req, res) => {
+  try {
+    const songs = await Song.aggregate([
+      {
+        $sample: { size: 4 },
+      },
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          artist: 1,
+          coverImage: 1,
+          duration: 1,
+          url: 1,
+        },
+      },
+    ]);
+    res.status(200).json({
+      success: true,
+      message: "Featured songs fetched successfully",
+      songs,
+    });
+  } catch (error) {
+    console.error("Error in fetching trending songs:", error);
+    res.status(500).json({
+      success: false,
+      message: "An unexpected error occurred while fetching trending songs.",
+    });
+  }
+};
+
+export const searchSongs = async (req, res) => {
+  try {
+    const { name } = req.query;
+    if (!name || name.trim() === "") {
+      return res.status(400).json({
+        success: false,
+        message:
+          "Query parameter 'name' is required and must be a non-empty string for searching songs.",
+      });
+    }
+    const searchRegex = new RegExp(name.trim(), "i");
+    const songs = await Song.find({
+      $or: [
+        { title: { $regex: searchRegex } },
+        { artist: { $regex: searchRegex } },
+      ],
+    }).select("_id title artist coverImage duration url");
+    res.status(200).json({
+      success: true,
+      message: "Songs fetched successfully",
+      songs,
+    });
+  } catch (error) {
+    console.error("Error in searching songs:", error);
+    res.status(500).json({
+      success: false,
+      message: "An unexpected error occurred while searching songs.",
+    });
+  }
+};
--- src/routes/song.route.js
+++ src/routes/song.route.js
@@ -1,5 +1,16 @@
 import express from "express";
-
+import {
+  getAllSongs,
+  trendingSongs,
+  getFeaturedSongs,
+  searchSongs,
+} from "../controllers/song.controller.js";
+import { protect, adminOnly } from "../middlewares/auth.middleware.js";
 const songRouter = express.Router();
+
+songRouter.get("/", protect, adminOnly, getAllSongs);
+songRouter.get("/trending", trendingSongs);
+songRouter.get("/featured", getFeaturedSongs);
+songRouter.get("/search", searchSongs);
 
 export default songRouter;
