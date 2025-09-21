a470d0986dab92c9cef19433daeb38ed68d1aa9a
Harshit_Parmar <parmarharshit441@gmail.com>
2025-09-21 21:18:52 +0530

    setup song router

--- src/server.js
+++ src/server.js
@@ -33,10 +33,12 @@ app.use(
 
 import authRouter from "./routes/auth.route.js";
 import adminRouter from "./routes/admin.route.js";
+import songRouter from "./routes/song.route.js";
 
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/admin",adminRouter);
+app.use("/api/v1/songs",songRouter);
 
 // server started
 connectDb()
