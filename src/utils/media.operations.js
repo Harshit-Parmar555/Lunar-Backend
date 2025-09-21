7aacd6c0494e52350813ba18645b752d4302b4ba
Harshit_Parmar <parmarharshit441@gmail.com>
2025-09-21 19:32:53 +0530

    add upload function for  firebase

---
+++ src/utils/media.operations.js
@@ -0,0 +1,30 @@
+import fs from "fs";
+import { bucket } from "../utils/firebase.config.js";
+
+const uploadImageToFirebase = async (file) => {
+  try {
+    if (!file) {
+      throw new Error("No file provided");
+    }
+
+    const localFilePath = file.path;
+    const uniqueFileName = `${Date.now()}_${file.originalname}`;
+    const firebaseFile = bucket.file(uniqueFileName);
+
+    await firebaseFile.save(fs.readFileSync(localFilePath), {
+      metadata: { contentType: file.mimetype },
+    });
+
+    await firebaseFile.makePublic();
+    const publicUrl = firebaseFile.publicUrl();
+
+    fs.unlinkSync(localFilePath);
+
+    return publicUrl;
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    throw error;
+  }
+};
+
+export { uploadImageToFirebase };
