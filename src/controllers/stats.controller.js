519c005b38870fa29ea27cfd088cdf84115601c6
Harshit_Parmar <parmarharshit441@gmail.com>
2025-09-24 08:40:33 +0530

    add artist count and change the response of the stats controller

--- src/controllers/stats.controller.js
+++ src/controllers/stats.controller.js
@@ -4,15 +4,38 @@ import User from "../models/user.model.js";
 
 export const getStats = async (req, res) => {
   try {
-    const [totalSongs, totalAlbums, totalUsers] = await Promise.all([
-      Song.countDocuments(),
-      Album.countDocuments(),
-      User.countDocuments(),
-    ]);
-    res.status(200).json({
-      success: true,
-      data: { totalSongs, totalAlbums, totalUsers },
-    });
+    const [totalSongs, totalAlbums, totalUsers, uniqueArtists] =
+      await Promise.all([
+        Song.countDocuments(),
+        Album.countDocuments(),
+        User.countDocuments(),
+
+        Song.aggregate([
+          {
+            $unionWith: {
+              coll: "albums",
+              pipeline: [],
+            },
+          },
+          {
+            $group: {
+              _id: "$artist",
+            },
+          },
+          {
+            $count: "count",
+          },
+        ]),
+      ]);
+
+    res
+      .status(200)
+      .json({
+        totalAlbums,
+        totalSongs,
+        totalUsers,
+        totalArtists: uniqueArtists[0]?.count || 0,
+      });
   } catch (error) {
     console.error("Error in fetching stats:", error);
     res.status(500).json({
